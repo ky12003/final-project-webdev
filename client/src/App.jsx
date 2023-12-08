77336df6ddd0266e3fc33a0ce27a242734b6b57c
Kenta Yamamoto <kenta12003@gmail.com>
2023-12-08 04:58:49 -0800

    Attempt at integrating frontend with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,24 +25,17 @@ import * as classes from './scripts/classes.js'
 function App() {
   const [backendData, setBackendData] = useState([{}])
 
-  
+  useEffect(() => {
+    fetch("/").then(
+      response => response.json
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
 
-  // app.post('/generate-plan', (req, res) => {
-  //     const major = req.body.major;
-  //     if (!major || !majorsData[major]) {
-  //         return res.status(400).send("Invalid or missing major");
-  //     }
 
-  //     let plan = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: []};
-  //     const majorData = majorsData[major];
-
-  //     classes.addLowerDivisionCourses(majorData, plan);
-  //     classes.addUpperDivisionCourses(majorData, plan);
-  //     classes.addBreadthCourses(majorData, majorData.breadth_courses, plan);
-  //     classes.fillWithElectives(plan);
-
-  //     res.json({ message: "Plan generated for " + major, plan: plan });
-  // });
 
   return (
     <>
--- server/app.js
+++ server/app.js
@@ -7,26 +7,27 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.get('/', (req, res) => {
   res.send('Welcome to BearPlanner!');
+  res.json({"users": ["hswer"]})
 });
 
-// app.post('/generate-plan', (req, res) => {
-//     const major = req.body.major;
-//     if (!major || !majorsData[major]) {
-//         return res.status(400).send("Invalid or missing major");
-//     }
+app.post('/generate-plan', (req, res) => {
+    const major = req.body.major;
+    if (!major || !majorsData[major]) {
+        return res.status(400).send("Invalid or missing major");
+    }
 
-//     let plan = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: []};
-//     const majorData = majorsData[major];
+    let plan = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: []};
+    const majorData = majorsData[major];
 
-//     addLowerDivisionCourses(majorData, plan);
-//     addUpperDivisionCourses(majorData, plan);
-//     addBreadthCourses(majorData, majorData.breadth_courses, plan);
-//     fillWithElectives(plan);
+    // addLowerDivisionCourses(majorData, plan);
+    // addUpperDivisionCourses(majorData, plan);
+    // addBreadthCourses(majorData, majorData.breadth_courses, plan);
+    // fillWithElectives(plan);
 
-//     res.json({ message: "Plan generated for " + major, plan: plan });
-// });
+    res.json({ message: "Plan generated for " + major, plan: plan });
+});
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
