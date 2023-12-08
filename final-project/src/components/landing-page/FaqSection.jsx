52b64626476f45ba1f6f8206a320ccb77a67aea5
Kenta Yamamoto <kenta12003@gmail.com>
2023-12-08 01:48:10 -0800

    Added FAQ Section

--- final-project/src/components/landing-page/FaqSection.jsx
+++ final-project/src/components/landing-page/FaqSection.jsx
@@ -1,12 +1,61 @@
-import "../../css/LandingPage.css"
+import "../../css/LandingPage.css";
 
 const FaqSection = () => {
+  return (
+    <>
+      <section id="faq-section" class="section">
+        <div id="faq-content-wrap">
+          <h1 id="faq-title">Frequently Asked Questions</h1>
 
-    return (
-      <>
-        
-      </>
-    );
-  }
-  
-  export default FaqSection;
+          <div class="faq-item">
+            <div class="faq-label">
+              <h2>Q:</h2>
+            </div>
+            <div class="faq-content">
+              <p class="question">What services does BearPlanner offer?</p>
+            </div>
+          </div>
+
+          <div class="faq-item">
+            <div class="faq-label">
+              <h2>A:</h2>
+            </div>
+            <div class="faq-content">
+              <p class="answer">
+                BearPlanner provides a comprehensive course planning platform
+                tailored for Berkeley students. Our services include schedule
+                optimization, degree tracking, and personalized academic
+                guidance.
+              </p>
+            </div>
+          </div>
+
+          <div class="faq-item">
+            <div class="faq-label">
+              <h2>Q:</h2>
+            </div>
+            <div class="faq-content">
+              <p class="question">How can I get started with BearPlanner?</p>
+            </div>
+          </div>
+
+          <div class="faq-item">
+            <div class="faq-label">
+              <h2>A:</h2>
+            </div>
+            <div class="faq-content">
+              <p class="answer">
+                To get started, simply create an account on our platform. Once
+                logged in, you can input your courses, explore scheduling
+                options, and take advantage of our user-friendly tools designed
+                to streamline your academic planning experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FaqSection;
--- final-project/src/components/landing-page/SectionTwo.jsx
+++ final-project/src/components/landing-page/SectionTwo.jsx
@@ -4,7 +4,30 @@ const SectionTwo = () => {
 
     return (
       <>
-
+        <section id="section2" className="section">
+          <div id="section2-content">
+            <div id="text">
+              <h1 id="mission">Mission Statement</h1>
+              <p id="more">
+                At BearPlanner, we are dedicated to empowering Berkeley students
+                on their academic journey. Our mission is to simplify and
+                enhance the course planning experience, providing a
+                user-friendly platform tailored to the unique needs of the
+                Berkeley community. We strive to foster a sense of academic
+                confidence by offering intuitive tools that enable students to
+                navigate complex schedules, make informed decisions, and
+                optimize their educational path. With a commitment to innovation
+                and accessibility, BearPlanner aims to be the go-to resource for
+                Berkeley Bears, supporting them in achieving their academic and
+                career goals.{" "}
+              </p>
+            </div>
+            <img
+              id="image2"
+              src="https://www.gettingsmart.com/wp-content/uploads/2017/05/Graduate-Readiness.jpg"
+            />
+          </div>
+        </section>
       </>
     );
   }
